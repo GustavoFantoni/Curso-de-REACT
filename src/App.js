bc563259578e88b2e9d486a36d8ce41eabef48a1
Gustavo Fantoni <gustavo.rosario@unifebe.edu.br>
2024-01-25 10:25:04 -0300

    Curso REACT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function soma(a, b) {
+    return a + b
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello, world!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p> soma= {soma(5, 5)}</p>
     </div>
   );
 }
